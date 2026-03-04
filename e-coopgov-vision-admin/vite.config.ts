4bc5982574dcdf1f0f5288a0878cdd8a2303aa29
root <root@doc-xcsl.vietnix.vn>
2026-03-04 09:07:35 +0700

    setup frontends

--- e-coopgov-vision-admin/vite.config.ts
+++ e-coopgov-vision-admin/vite.config.ts
@@ -3,21 +3,25 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
     hmr: {
       overlay: false,
     },
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://api.ecoopgov.devhub.ai.vn",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+    allowedHosts: [
+      "admin.ecoopgov.devhub.ai.vn",
+      // nếu chạy qua subdomain khác sau này thì có thể dùng dạng wildcard:
+      // ".ecooppgov.devhub.ai.vn"
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
